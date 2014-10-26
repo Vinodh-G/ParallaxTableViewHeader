363fdb4cbb46cee0d8c1d9d95fac770b0ecc930a
Daston <vinodh.g.dev@gmail.com>
2014-10-26 13:09:52 +0530

    Initial Commit

---
+++ ParallaxTableViewHeader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParallaxTableViewHeader
+//
+//  Created by Vinodh  on 26/10/14.
+//  Copyright (c) 2014 Daston~Rhadnojnainva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ParallaxTableViewHeader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ParallaxTableViewHeader
+//
+//  Created by Vinodh  on 26/10/14.
+//  Copyright (c) 2014 Daston~Rhadnojnainva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
