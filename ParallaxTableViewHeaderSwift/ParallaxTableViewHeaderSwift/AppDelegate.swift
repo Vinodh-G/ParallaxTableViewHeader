c07a3eb06a95d0c9dd1c4afbe3e0db15b74537e9
Christopher Reitz <christopher@reitz.re>
2016-04-24 11:46:07 +0200

    Add Swift Port

---
+++ ParallaxTableViewHeaderSwift/ParallaxTableViewHeaderSwift/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  ParallaxTableViewHeaderSwift
+//
+//  Created by Christopher Reitz on 24.04.16.
+//  Copyright © 2016 Christopher Reitz. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
---
+++ ParallaxTableViewHeaderSwift/ParallaxTableViewHeaderSwift/ParallaxHeaderView.swift
@@ -0,0 +1,166 @@
+//
+//  ParallaxHeaderView.swift
+//  ParallaxTableViewHeaderSwift
+//
+//  Created by Christopher Reitz on 24.04.16.
+//  Copyright © 2016 Christopher Reitz. All rights reserved.
+//
+
+import UIKit
+
+class ParallaxHeaderView: UIView {
+
+    // MARK: - Properties
+
+    var headerImage = UIImage() {
+        didSet {
+            imageView.image = headerImage
+            refreshBlurViewForNewImage()
+        }
+    }
+
+    var headerTitleLabel = UILabel()
+    var imageScrollView = UIScrollView()
+    var imageView = UIImageView()
+    var bluredImageView = UIImageView()
+    var subView: UIView?
+
+    var kDefaultHeaderFrame = CGRect()
+    let kParallaxDeltaFactor: CGFloat = 0.5
+    let kMaxTitleAlphaOffset: CGFloat = 100.0
+    let kLabelPaddingDist: CGFloat = 8.0
+
+    // MARK: - Initializer
+
+    init(image: UIImage, forSize headerSize: CGSize) {
+        super.init(frame: CGRectMake(0, 0, headerSize.width, headerSize.height))
+
+        kDefaultHeaderFrame = CGRectMake(0, 0, frame.size.width, frame.size.height)
+
+        headerImage = image
+        initialSetupForDefaultHeader()
+    }
+
+    init(subView: UIView) {
+        super.init(frame: CGRectMake(0, 0, subView.frame.size.width, subView.frame.size.height))
+
+        kDefaultHeaderFrame = CGRectMake(0, 0, frame.size.width, frame.size.height)
+
+        initialSetupForCustomSubView(subView)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        if let subView = self.subView {
+            initialSetupForCustomSubView(subView)
+        }
+        else {
+            initialSetupForDefaultHeader()
+        }
+    }
+
+    // MARK: - Public
+
+    func layoutHeaderViewForScrollViewOffset(offset: CGPoint) {
+        if offset.y > 0 {
+            var frame = imageScrollView.frame
+            frame.origin.y = max(offset.y * kParallaxDeltaFactor, 0)
+            imageScrollView.frame = frame
+            bluredImageView.alpha = 1 / kDefaultHeaderFrame.size.height * offset.y * 2
+            clipsToBounds = true
+        }
+        else {
+            let delta: CGFloat = fabs(min(0.0, offset.y))
+            var rect = kDefaultHeaderFrame
+            rect.origin.y -= delta
+            rect.size.height += delta
+            imageScrollView.frame = rect
+            clipsToBounds = false
+            headerTitleLabel.alpha = 1 - delta * 1 / kMaxTitleAlphaOffset
+        }
+    }
+
+    func refreshBlurViewForNewImage() {
+        var screenShot = screenShotOfView(self)
+        screenShot = screenShot.applyBlurWithRadius(5, tintColor: UIColor.whiteColor().colorWithAlphaComponent(0.2), saturationDeltaFactor: 1.0, maskImage: nil)!
+        bluredImageView.image = screenShot
+    }
+
+    // MARK: - Private
+
+    private func initialSetupForDefaultHeader() {
+        imageScrollView = UIScrollView(frame: bounds)
+        imageView = UIImageView(frame: imageScrollView.bounds)
+        imageView.autoresizingMask = [
+            .FlexibleLeftMargin,
+            .FlexibleRightMargin,
+            .FlexibleTopMargin,
+            .FlexibleBottomMargin,
+            .FlexibleHeight,
+            .FlexibleWidth
+        ]
+        imageView.contentMode = .ScaleAspectFill
+        imageView.image = headerImage
+        imageScrollView.addSubview(imageView)
+
+        var labelRect = imageScrollView.bounds
+        labelRect.origin.x = kLabelPaddingDist
+        labelRect.origin.y = kLabelPaddingDist
+        labelRect.size.width = labelRect.size.width - 2 * kLabelPaddingDist
+        labelRect.size.height = labelRect.size.height - 2 * kLabelPaddingDist
+
+        headerTitleLabel = UILabel(frame: labelRect)
+        headerTitleLabel.textAlignment = .Center
+        headerTitleLabel.numberOfLines = 0
+        headerTitleLabel.lineBreakMode = .ByWordWrapping
+        headerTitleLabel.autoresizingMask = imageView.autoresizingMask
+        headerTitleLabel.textColor = UIColor.whiteColor()
+        headerTitleLabel.font = UIFont(name: "AvenirNextCondensed-Regular", size: 23)
+        imageScrollView.addSubview(headerTitleLabel)
+
+        bluredImageView = UIImageView(frame: imageView.frame)
+        bluredImageView.autoresizingMask = imageView.autoresizingMask
+        bluredImageView.alpha = 0.0
+        imageScrollView.addSubview(bluredImageView)
+
+        addSubview(imageScrollView)
+        refreshBlurViewForNewImage()
+    }
+
+    private func initialSetupForCustomSubView(subView: UIView) {
+        let scrollView = UIScrollView(frame: bounds)
+        imageScrollView = scrollView
+        self.subView = subView
+        subView.autoresizingMask = [
+            .FlexibleLeftMargin,
+            .FlexibleRightMargin,
+            .FlexibleTopMargin,
+            .FlexibleBottomMargin,
+            .FlexibleHeight,
+            .FlexibleWidth
+        ]
+        imageScrollView.addSubview(subView)
+
+        bluredImageView = UIImageView(frame: subView.frame)
+        bluredImageView.autoresizingMask = subView.autoresizingMask
+        bluredImageView.alpha = 0.0
+        imageScrollView.addSubview(bluredImageView)
+
+        addSubview(imageScrollView)
+        refreshBlurViewForNewImage()
+    }
+
+    private func screenShotOfView(view: UIView) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(kDefaultHeaderFrame.size, true, 0.0)
+        drawViewHierarchyInRect(kDefaultHeaderFrame, afterScreenUpdates: false)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return image
+    }
+}
